7b8a2cecff85c2805229550a6938d64d6815e837
yyq <yyq@yyqdeMac-mini.local>
2025-04-22 11:01:12 +0800

    增加errorboundary组件

---
+++ src/components/error-boundary.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+
+interface ErrorBoundaryProps {
+  fallback: React.ReactNode;
+  children: React.ReactNode;
+}
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    // 更新状态，以便下一次渲染将显示后备 UI。
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // 你可以渲染任何自定义后备 UI
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
