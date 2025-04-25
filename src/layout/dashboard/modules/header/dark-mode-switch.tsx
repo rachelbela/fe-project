96f81ec4ab54cb9f82fa05ddbda870d187f05428
yyq <yyq@yyqdeMac-mini.local>
2025-04-25 10:38:52 +0800

    黑暗模式跟随系统

--- src/layout/dashboard/modules/header/dark-mode-switch.tsx
+++ src/layout/dashboard/modules/header/dark-mode-switch.tsx
@@ -1,41 +1,56 @@
-import { useState } from "react";
-import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
+import { useEffect, useRef, useState } from "react";
 import { Icon } from "@iconify/react";
+import { Button } from "@/components/ui/button";
 
 function DarkModeSwitch() {
-  const [dark, setDark] = useState(false);
+  const [value, setValue] = useState(false);
+  const htmlRef = useRef<HTMLHtmlElement | null>(null);
 
-  const handleToggle = (value: boolean) => {
+  useEffect(() => {
+    const isDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    setValue(isDarkMode);
     const el = document.getElementsByTagName("html")[0];
-    setDark(value);
     if (el) {
+      htmlRef.current = el;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (htmlRef.current) {
       if (value) {
-        el.classList.add("dark");
-        el.classList.remove("light");
-        el.setAttribute("style", "color-scheme: dark");
+        // 黑暗模式
+        htmlRef.current.classList.add("dark");
+        htmlRef.current.classList.remove("light");
+        htmlRef.current.setAttribute("style", "color-scheme: dark");
       } else {
-        el.classList.add("light");
-        el.classList.remove("dark");
-        el.setAttribute("style", "color-scheme: light");
+        // 白天模式
+        htmlRef.current.classList.add("light");
+        htmlRef.current.classList.remove("dark");
+        htmlRef.current.setAttribute("style", "color-scheme: light");
       }
     }
+  }, [value, htmlRef.current]);
+
+  const handleToggle = () => {
+    const newValue = !value;
+    setValue(newValue);
   };
   return (
     <div className="flex justify-end items-center">
-      <Switch
-        id="airplane-mode"
-        checked={dark}
-        onCheckedChange={handleToggle}
-        className="mr-1"
-      />
-      <Label htmlFor="airplane-mode">
-        {dark ? (
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={handleToggle}
+        className="w-7 h-7"
+      >
+        {value ? (
           <Icon icon="tabler:moon" width="16" height="16" />
         ) : (
           <Icon icon="flowbite:sun-outline" width="16" height="16" />
         )}
-      </Label>
+      </Button>
     </div>
   );
 }
